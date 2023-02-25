7818cf5bbbb1e013c0090f9d57b67fb23377e96f
verovaleros <vero.valeros@gmail.com>
2023-02-25 12:43:07 +0100

    Bashly init

---
+++ src/generate_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/generate_command.sh'"
+echo "# code for 'rrdcap generate' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args

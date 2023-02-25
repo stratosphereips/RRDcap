fc97a8f5162c9549535beda7d784ee1a01fe5f70
verovaleros <vero.valeros@gmail.com>
2023-02-25 21:32:45 +0100

    generate: implement generate rrd

--- src/generate_command.sh
+++ src/generate_command.sh
@@ -1,3 +1,18 @@
 # This tool is part of the Stratosphere Laboratory, AIC, FEL, CTU in Prague
 # Contact: stratosphere@aic.fel.cvut.cz
-:
+
+RACONFIG_FILE=${args[--config]}
+BIGARGUS_FILE=${args[--biargus]}
+
+RRD_FILE=${args[--rrd]}
+
+echo "[+] Updating $RRD_FILE with data from $BIGARGUS_FILE"
+
+ra -F "$RACONFIG_FILE" -n -Z b -r "$BIARGUS_FILE" | BotSummarizer.py -l -w 1 -r "$RRDFILE" -f - | rrdtool -
+
+if eval validate_file_exists "$RRD_FILE";
+then
+    echo "[+] Finished updating: $RRD_FILE"
+else
+    echo "[!] An error occurred updating the RRD file"
+fi

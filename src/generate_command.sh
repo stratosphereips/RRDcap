c979d2a42dec8ad5be8b4ca79d90491cbe4838bf
verovaleros <vero.valeros@gmail.com>
2023-02-25 13:17:27 +0100

    rrdcap generate: read and parse config parameters

--- src/generate_command.sh
+++ src/generate_command.sh
@@ -2,6 +2,12 @@
 # Contact: stratosphere@aic.fel.cvut.cz
 
 PCAP_FILE=${args[--pcap]}
-RRD_CONFIG=${args[--config]}
+CONFIG_FILE=${args[--config]}
+
+# Get RRD parameters from config file
+rrd_step=$(config_get "STEP")
+rrd_format=$(config_get "RRD_FORMAT")
 
 echo "[+] Creating RRD file for packet capture: $PCAP_FILE"
+echo "[+] RRD step configuration: $rrd_step"
+echo "[+] RRD format configuration: $rrd_format"
